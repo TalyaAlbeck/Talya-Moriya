5d5068c318c3e566ac4b4e5c9abe481770e014a5
Moriya <moriyakatsman@hilma.tech>
2024-10-14 11:35:47 +0300

    server post function

--- server-want-to-die/server.js
+++ server-want-to-die/server.js
@@ -1,21 +1,35 @@
-
-
 function checkRequestType(requestObj) {
     if (requestObj.request === "get") {
-        get(requestObj.url)
+        return get(requestObj.url);
     } else if (requestObj.request === "post") {
-        post(requestObj.url, requestObj.content)
+        post(requestObj.url, requestObj.content);
+        return 200;
     } else if (requestObj.request === "put") {
-        put(requestObj.url, requestObj.content)
+        put(requestObj.url, requestObj.content);
     } else {
         console.log("this kind of request is not defind");
     }
 }
 
 function get(url) {
-
+    const splitUrl = url.split("/");
+    return splitUrl[2];//calls the data base function and sends the result back to the client
 }
 
 function post(url, content) {
-
+    const splitUrl = url.split("/");
+    console.log('splitUrl: ', splitUrl[2]);
+    console.log(typeof splitUrl[2]);
+    switch(splitUrl[2]){
+       case "addUser": 
+        addUser(content.userName, content.password)
+       break;
+       default:
+        return false;
+    };//calls the data base function
+    
+    
 }
+post("http/users/addUser",{userName:'Moriya',password:12345,});
+
+
