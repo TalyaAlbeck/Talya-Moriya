b979ff58986d882690598fdcc484e9b75b2a60e0
Moriya <moriyakatsman@hilma.tech>
2024-10-14 17:15:07 +0300

    spa.js file

---
+++ server-want-to-die/spa.js
@@ -0,0 +1,19 @@
+const container = document.getElementsByClassName("container")[0];
+
+function showPage(tem) {
+    const template = document.querySelector(tem);
+    container.innerHTML = ``;
+    const clone = template.content.cloneNode(true)
+    container.appendChild(clone);
+    if (tem==="#log-in-page"){
+        
+        document.getElementById("log-in").addEventListener("click", logInButton)
+        document.getElementById("sign-up").addEventListener("click", signUpButton) 
+    }else if(tem==="#list"){
+       
+        document.getElementById("addT").addEventListener("click", addTask)
+    }
+}
+
+// showPage("#log-in-page")
+showPage("#list")
